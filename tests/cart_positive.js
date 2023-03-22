744b1e28a9baea972916ab32d95d4b943743af1e
Filip <dr.ipi95@gmail.com>
2023-03-22 13:48:52 +0100

    Arguments and parameters equalized

--- tests/cart_positive.js
+++ tests/cart_positive.js
@@ -1,12 +1,9 @@
-//import { checkoutCredentials } from "./checkout_credentials"
-
-async function cartPositive(page/*,firstName, lastName, zipCode*/) {
+async function cartPositive(page, firstName, lastName, zipCode) {
     await page.locator("#shopping_cart_container").click()
     await page.locator("#checkout").click()
-    //await checkoutCredentials(page, "Pera", "Peric", "11000")
-    await page.locator("#first-name").type("Pera")
-    await page.locator("#last-name").type("Peric")
-    await page.locator("#postal-code").type("11000")
+    await page.locator("#first-name").type(firstName)
+    await page.locator("#last-name").type(lastName)
+    await page.locator("#postal-code").type(zipCode)
     await page.locator("#continue").click()
     await page.locator("#finish").click()
     await page.locator("#back-to-products").click()
