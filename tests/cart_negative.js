edc626e2ba37cbc4647a836306f813aaad25acbb
Filip <dr.ipi95@gmail.com>
2023-03-22 13:24:28 +0100

    Updated verification for empty cart

--- tests/cart_negative.js
+++ tests/cart_negative.js
@@ -1,6 +1,9 @@
+import { expect } from "@playwright/test"
+
 async function cartNegative(page) {
     await page.locator("#shopping_cart_container").click()
-    //fali kod koji ce potvrditi da nema nista u cart-u
+    let checkCartEmpty = page.locator(".cart_list")
+    await expect(checkCartEmpty).not.toHaveText("Sauce Labs Backpack, Sauce Labs Bike Light, Sauce Labs Fleece Jacket, Sauce Labs Bolt T-Shirt, Sauce Labs Onesie, Test.allTheThings() T-Shirt (Red)")
     await page.locator("#continue-shopping").click()
 }
 
