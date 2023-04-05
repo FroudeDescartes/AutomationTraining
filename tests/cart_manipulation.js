7360c83588659fd6ba02545a39cbe05357f55353
Filip <dr.ipi95@gmail.com>
2023-04-05 18:12:50 +0200

    Added assertions

--- tests/cart_manipulation.js
+++ tests/cart_manipulation.js
@@ -7,8 +7,9 @@ async function stickyAddToCart(page) {
     await page.locator("#add-to-cart-sauce-labs-fleece-jacket").click()
     await page.locator("#add-to-cart-sauce-labs-onesie").click()
     await page.locator('[data-test="add-to-cart-test\\.allthethings\\(\\)-t-shirt-\\(red\\)"]').click();
-    let addedToCart = page.locator(".shopping_cart_badge")
-    await expect(addedToCart).toContainText("6")
+    let addedToCart = await page.locator(".shopping_cart_badge").innerHTML()
+    let addedToCartNum = parseInt(addedToCart)
+    expect(addedToCartNum).toBe(6)
 }
 
 
@@ -19,6 +20,8 @@ async function removeFromCart(page) {
     for (let i = cartItemCount - 1; i >= 0; i--) {
         await page.locator(".cart_item button").nth(i).click()
     }
+    let noItem = page.locator(".cart_item")
+    await expect(noItem).toBeHidden()
     await page.locator("#continue-shopping").click()
 }
 
@@ -28,6 +31,9 @@ async function allItemsToCart(page) {
     for (let i = 0; i < itemCount; i++) {
         await page.locator(".inventory_item button").nth(i).click()
     }
+    let itemsInCart = await page.locator(".shopping_cart_badge").innerHTML()
+    let itemsInCartNum = parseInt(itemsInCart)
+    expect(itemsInCartNum).toBe(itemCount)
 }
 
 
@@ -35,7 +41,14 @@ async function randomItem(page) {
     let itemCount = await page.locator(".inventory_item").count()
     let randomItemNumber = Math.floor(Math.random() * itemCount)
     await page.locator(".inventory_item button").nth(randomItemNumber).click()
+
+    await page.locator("#shopping_cart_container").click()
+    let itemAdded = page.locator(".cart_item")
+    await expect(itemAdded).toBeVisible()
+    await page.locator("#continue-shopping").click()
 }
 
 
 export { stickyAddToCart, removeFromCart, allItemsToCart, randomItem }
+
+//every repetitive task needs to be made as separate function and then called in places where it need to occur, so that is my next goal
