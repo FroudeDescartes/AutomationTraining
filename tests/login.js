c721a8781ec3b7c7321df3a6344a38c7bf8dcafa
Filip <dr.ipi95@gmail.com>
2023-04-07 15:58:09 +0200

    Added assertions for credentials

--- tests/login.js
+++ tests/login.js
@@ -3,7 +3,19 @@ import { expect } from "@playwright/test"
 async function login(page, username, password = "secret_sauce") {
        await page.locator("#user-name").type(username)
        await page.locator("#password").type(password)
+       await expectUsername(page, username)
+       await expectPassword(page, password)
        await page.keyboard.press("Enter")
+}
+
+async function expectUsername(page, username) {
+       let loginUsername = await page.locator("#user-name").inputValue()
+       expect(loginUsername).toBe(username)
+}
+
+async function expectPassword(page, password) {
+       let loginPassword = await page.locator("#password").inputValue()
+       expect(loginPassword).toBe(password)
 }
 
 export { login }
